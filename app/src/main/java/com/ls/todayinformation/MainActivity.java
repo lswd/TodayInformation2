0e2fcb559ac6d7c2524704ed3e506f6652c6b69e
king <leijinbiao@foxmail.com>
2021-05-04 23:24:00 +0800

    提交测试2

--- app/src/main/java/com/ls/todayinformation/MainActivity.java
+++ app/src/main/java/com/ls/todayinformation/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("MainActivity", "onCreate: ");
+        Log.e("MainActivity", "onCreate: 测试2");
     }
 }
